3da8d3834a674facc70fc81ea43f56e8e6a7df30
David Keller <kellerdb@gmail.com>
2021-05-16 08:42:46 -0700

    formatting

--- script.js
+++ script.js
@@ -10,35 +10,30 @@ number.focus()
 function findPrimes() {
   let numberValue = number.value.trim()
 
-  if (numberValue != `` && !isNaN(numberValue)) {
-    if (numberValue < 2 || numberValue > 10000) {
-      box.innerHTML = `Upper bound must be between 2 and 10,000.`
-    }
-    else {
-      let crossedOut = []
+  if (numberValue < 2 || numberValue > 10000) {
+    box.innerHTML = `Upper bound must be between 2 and 10,000.`
+  } else {
+    let crossedOut = []
 
-      for (let i = 2; i * i <= numberValue; i++) {
-        if (!crossedOut[i]) {
-          for (let j = i * i; j <= numberValue; j += i) {
-            crossedOut[j] = true
-          }
+    for (let i = 2; i * i <= numberValue; i++) {
+      if (!crossedOut[i]) {
+        for (let j = i * i; j <= numberValue; j += i) {
+          crossedOut[j] = true
         }
       }
+    }
 
-      box.innerHTML = ``
+    box.innerHTML = ``
 
-      for (let i = 2; i <= numberValue; i++) {
-        if (!crossedOut[i]) {
-          let numberDiv = document.createElement(`div`)
-          numberDiv.classList.add(`number`)
-          numberDiv.innerHTML = i
-          box.appendChild(numberDiv)
-        }
+    for (let i = 2; i <= numberValue; i++) {
+      if (!crossedOut[i]) {
+        let numberDiv = document.createElement(`div`)
+        numberDiv.classList.add(`number`)
+        numberDiv.innerHTML = i
+        box.appendChild(numberDiv)
       }
     }
   }
-
-  number.focus()
 }
 
 function keyPressed(event) {
